01697c5cbd874a7c04f2f17dc397dd551300d0ad
unknown <bradbautista@gmail.com>
2020-03-03 00:56:42 -0500

    searches added

---
+++ search.js
@@ -0,0 +1,66 @@
+// indexOf is an example of a linear search
+
+function indexOf(array, value) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+};
+
+
+function binarySearch(array, value, start, end) {
+    var start = start === undefined ? 0 : start;
+    var end = end === undefined ? array.length : end;
+
+    if (start > end) {
+        return -1;
+    }
+
+    const index = Math.floor((start + end) / 2);
+    const item = array[index];
+
+    console.log(start, end);
+    if (item == value) {
+        return index;
+    }
+    else if (item < value) {
+        return binarySearch(array, value, index + 1, end);
+    }
+    else if (item > value) {
+        return binarySearch(array, value, start, index - 1);
+    }
+};
+
+dfs(values=[]) {
+    if (this.left) {
+        values = this.left.dfs(values);
+    }
+    values.push(this.value);
+
+    if (this.right) {
+        values = this.right.dfs(values);
+    }
+    return values;
+}
+
+bfs(tree, values = []) {
+    const queue = new Queue(); // Assuming a Queue is implemented
+    const node = tree.root;
+    queue.enqueue(node);
+    while (queue.length) {
+        const node = queue.dequeue(); //remove from the queue
+        values.push(node.value); // add that value from the queue to an array
+
+        if (node.left) {
+            queue.enqueue(node.left); //add left child to the queue
+        }
+
+        if (node.right) {
+            queue.enqueue(node.right); // add right child to the queue
+        }
+    }
+
+    return values;
+}
